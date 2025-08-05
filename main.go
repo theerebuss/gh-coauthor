fdf1e9b0d6ed09f40570c64af902a060995b3ceb
theerebuss <39736248+theerebuss@users.noreply.github.com>
2025-08-05 12:35:38 +0200

    feat: use cobra-cli

--- main.go
+++ main.go
@@ -2,25 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/cli/go-gh/v2/pkg/api"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-coauthor extension!")
-	client, err := api.DefaultRESTClient()
-	if err != nil {
-		fmt.Println(err)
-		return
+	var rootCmd = &cobra.Command{
+		Use:   "gh-coauthor",
+		Short: "A GitHub CLI extension for co-authors",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("hi world, this is the gh-coauthor extension!")
+			client, err := api.DefaultRESTClient()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			response := struct{ Login string }{}
+			err = client.Get("user", &response)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Printf("running as %s\n", response.Login)
+		},
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
-}
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
