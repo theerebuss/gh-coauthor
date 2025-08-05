778d204186652e54a2ac414f1d887bdda0245b35
theerebuss <39736248+theerebuss@users.noreply.github.com>
2025-08-05 14:16:08 +0200

    feat: add co-author for specific user login

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/cli/go-gh/v2/pkg/api"
+	"github.com/go-git/go-git/v6"
 	"github.com/spf13/cobra"
 )
 
@@ -15,12 +18,95 @@ var rootCmd = &cobra.Command{
 	Example: `gh coauthor
 gh coauthor johndoe`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) > 0 {
-			fmt.Println(args[0])
-		} else {
-			fmt.Println("hi world, this is the gh-coauthor extension!")
+		if len(args) > 1 {
+			return
 		}
+
+		if len(args) == 0 {
+			fmt.Println("Missing implementation.")
+			return
+		}
+
+		var authorInfo string
+		if len(args) == 1 {
+			ai, err := getAuthorInfo(args[0])
+			if err != nil {
+				fmt.Println("Error getting author info:", err)
+				return
+			}
+			authorInfo = ai
+		}
+
+		addCoauthorToLastCommit(authorInfo)
 	},
+}
+
+func getAuthorInfo(username string) (string, error) {
+	client, err := api.DefaultRESTClient()
+	if err != nil {
+		log.Fatal(err)
+		return "", fmt.Errorf("failed to create API client: %w", err)
+	}
+
+	res := struct {
+		Id    int64  `json:"id"`
+		Name  string `json:"name"`
+		Login string `json:"login"`
+		Email string `json:"email"`
+	}{}
+	err = client.Get("users/"+username, &res)
+	if err != nil {
+		log.Fatal(err)
+		return "", fmt.Errorf("failed to get user info: %w", err)
+	}
+
+	var name string
+	if res.Name == "" {
+		name = res.Login
+	} else {
+		name = res.Name
+	}
+
+	var email string
+	if res.Email == "" {
+		email = fmt.Sprintf("%d+%s@users.noreply.github.com", res.Id, res.Login)
+	} else {
+		email = res.Email
+	}
+
+	return fmt.Sprintf("%s <%s>", name, email), nil
+}
+
+func addCoauthorToLastCommit(authorInfo string) {
+	r, err := git.PlainOpen(".")
+	if err != nil {
+		fmt.Println("Error opening git repository:", err)
+		return
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		fmt.Println("Error getting worktree:", err)
+		return
+	}
+	h, err := r.Head()
+	if err != nil {
+		fmt.Println("Error getting repository head:", err)
+		return
+	}
+
+	c, err := r.CommitObject(h.Hash())
+	if err != nil {
+		fmt.Println("Error getting latest commit:", err)
+		return
+	}
+
+	newMessage := fmt.Sprintf("%s\n\nCo-authored-by: %s", c.Message, authorInfo)
+	w.Commit(newMessage, &git.CommitOptions{
+		Amend: true,
+	})
+
+	fmt.Printf("Added co-author: %s\n", authorInfo)
 }
 
 func Execute() {
--- go.mod
+++ go.mod
@@ -5,21 +5,37 @@ go 1.24.5
 require github.com/cli/go-gh/v2 v2.12.1
 
 require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/ProtonMail/go-crypto v1.3.0 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/cli/safeexec v1.0.0 // indirect
 	github.com/cli/shurcooL-graphql v0.0.4 // indirect
+	github.com/cloudflare/circl v1.6.1 // indirect
+	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg/v2 v2.0.2 // indirect
+	github.com/go-git/go-billy/v6 v6.0.0-20250627091229-31e2a16eef30 // indirect
+	github.com/go-git/go-git/v6 v6.0.0-20250728093604-6aaf1933ecab // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/henvic/httpretty v0.0.6 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/pjbgf/sha1cd v0.4.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sergi/go-diff v1.4.0 // indirect
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/term v0.30.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/exp v0.0.0-20250531010427-b6e5de432a8b // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/term v0.33.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
